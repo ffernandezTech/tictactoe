7a809d7e3a51ceecd33e18051acb86542bde673a
ffrndzTech <199731008+ffernandezTech@users.noreply.github.com>
2025-10-17 02:54:21 -0700

    feat: funciton

---
+++ script.js
@@ -0,0 +1,33 @@
+const startGame = () =>{
+    const gameBoard = [
+        1,2,3,
+        4,5,6,
+        7,8,9
+
+    ];
+
+    const result = ()=>{
+        console.log('hello');
+    }
+    const display = ()=>{
+        let row =[];
+        let units ='';
+        let i = 0;
+        gameBoard.forEach(cell => {
+            units.
+            if(cell%3 === 0)
+            {
+
+            }
+            
+        });
+    }
+
+    return {display, result};
+
+
+}
+
+const start = startGame();
+start.display();
+
